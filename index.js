8cad797a5536f5bef49919383b6e20482d7a76fd
Saloni <salonimehta2127@gmail.com>
2021-04-27 16:25:45 -0400

    commit tree a10ddf6be4c65b519335e675b061cf5a9b2e2e1e

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-
-// Write your code here
+const num1=31,num2=2,num3=9000,num4=13;
+const multiply=num1*num2;
+const random=Math.floor(Math.random()*20)+1;
+const mod= num3%num4;
+const max= Math.max(10,15,16,20);
